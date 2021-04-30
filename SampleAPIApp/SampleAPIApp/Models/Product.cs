4fe8aa15d2ad78fdb66e4dad91d8be330adf0703
lastar38 <lastarmain@gmail.com>
2021-04-30 22:37:23 +0900

    新規作成

---
+++ SampleAPIApp/SampleAPIApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleAPIApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        public string Description { get; set; }
+    }
+}
